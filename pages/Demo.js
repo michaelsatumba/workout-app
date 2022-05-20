cce674ee4c047660190c1632d3943b2fc0a78590
Anmol Burmy <anmolburmy145@gmail.com>
2022-05-20 14:39:59 -0700

    demo page done

--- pages/Demo.js
+++ pages/Demo.js
@@ -3,7 +3,71 @@ export default function Home() {
     return (
         <div>
             <NavBar />
-            <img src="/Demo.gif" alt="funny animation GIF" className="demo" width="15%" />
+            <div className="figma">
+                <iframe
+                    width="500"
+                    height="850"
+                    src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FntnAKy4CJbuNgybXEhpzGj%2FClass-Activity-1%253A-Wireframing-and-Protoyping%3Fnode-id%3D133%253A192%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D133%253A192"
+                ></iframe>
+            </div>
+            <div className="flex items-center justify-evenly demo-cont">
+                <img src="/Demo.gif" alt="funny animation GIF" className="demo" width="15%" />
+
+                <div className="demo-info">
+                    <div className="demo-info-cont">
+                        <div className="demo-info-wrap">
+                            <h1 className="font-black text-3xl my-5">Feed Page</h1>
+                            <p className="font-black text-1xl pr-16">
+                                The feed page is be the first page the user is see whenever they open the application. This page
+                                is contain a feed of all the people/pages followed by the user. These feeds may include images of
+                                personal records, workout tips, gym sessions information etc. There is also a description at the
+                                bottom of the image along with a link to the profile of the person who posted the image. Users can
+                                scroll to view more posts.
+                            </p>
+                        </div>
+                        <img src="/Feed.png" alt="funny animation GIF" className="demo-info-img" width="27%" />
+                    </div>
+
+                    <div className="demo-info-cont">
+                        <div className="demo-info-wrap">
+                            <h1 className="font-black text-3xl my-5">Calendar Page</h1>
+                            <p className="font-black text-1xl pr-16">
+                                Users want an easy way to just keep track of their exercise they’ve done. Users can choose and
+                                record a type of exercise they have done on a particular day. They can also mark on the calendar
+                                to keep track of the days they missed to workout.
+                            </p>
+                        </div>
+                        <img src="/Calender.png" alt="funny animation GIF" className="demo-info-img" width="27%" />
+                    </div>
+
+                    <div className="demo-info-cont">
+                        <div className="demo-info-wrap">
+                            <h1 className="font-black text-3xl my-5">Discover Page</h1>
+                            <p className="font-black text-1xl pr-16">
+                                The discovery page will be used by users to find new workouts, diet plans and nearby gyms classes
+                                and sessions. This page will be adjusted according to the preference of the user. For example for
+                                a user who is a beginner, this page will show more easier and beginner friendly workouts, classes
+                                and diet plans compared to the more experienced users
+                            </p>
+                        </div>
+                        <img src="/Discover.png" alt="funny animation GIF" className="demo-info-img" width="27%" />
+                    </div>
+
+                    <div className="demo-info-cont">
+                        <div className="demo-info-wrap">
+                            <h1 className="font-black text-3xl my-5">Chat Page</h1>
+                            <p className="font-black text-1xl pr-16">
+                                The chat page is a simple and minimalist with displaying all the friends of the user. The user can
+                                interact with a profile tile to chat with that person. Furthermore at the bottom of the profile,
+                                there will be an indication which will show the user whether the person is available for a gym
+                                session. This way any user can set a preference that they are free to workout with a partner on a
+                                particular day
+                            </p>
+                        </div>
+                        <img src="/Chat.png" alt="funny animation GIF" className="demo-info-img" width="27%" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- pages/Research.js
+++ pages/Research.js
@@ -1,59 +1,48 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
+import React from "react";
+import NavBar from "../components/NavBar";
 
 function Research() {
-	return (
-		<div>
-			<NavBar />
+    return (
+        <div>
+            <NavBar />
 
-			<div className="grid grid-rows-4 grid-flow-col gap-4 my-10">
-				<div className="p-10">
-					<p className="font-medium text-3xl my-10">Design Research</p>
+            <div className="research">
+                <div className="p-10">
+                    <p className="font-medium text-3xl my-10">Design Research</p>
 
-					<p className="">
-						For our design research we sought out friends, family and close
-						acquaintances and had them fill out a survey. In terms of data we
-						wanted to get we wanted both quantitative and qualitative data.
-						Quantitative data such as if people prefer working out alone or with
-						people, if people use workout apps and such. Our qualitative data
-						would include something like having the participant describe what
-						their ideal workout app would be. After gathering this data using
-						Google forms we would organize it on a Miro Board and discuss it
-						with each other to find any patterns or things of interest. We would
-						also use the data Google Forms gave us to give us a general
-						quantitative idea of our data, such as what percent of participants
-						prefers to workout alone, or what percent of participants would like
-						fitness trainers. After developing a prototype using the data we
-						would then send it to some users to test it. We would ask the user
-						to record themselves using our website to see how they interact with
-						the prototype. With the recording we would find areas which the user
-						struggled with in our prototype in hopes of improving it.
-					</p>
-				</div>
+                    <p className="">
+                        For our design research we sought out friends, family and close acquaintances and had them fill out a
+                        survey. In terms of data we wanted to get we wanted both quantitative and qualitative data. Quantitative
+                        data such as if people prefer working out alone or with people, if people use workout apps and such. Our
+                        qualitative data would include something like having the participant describe what their ideal workout app
+                        would be. After gathering this data using Google forms we would organize it on a Miro Board and discuss it
+                        with each other to find any patterns or things of interest. We would also use the data Google Forms gave
+                        us to give us a general quantitative idea of our data, such as what percent of participants prefers to
+                        workout alone, or what percent of participants would like fitness trainers. After developing a prototype
+                        using the data we would then send it to some users to test it. We would ask the user to record themselves
+                        using our website to see how they interact with the prototype. With the recording we would find areas
+                        which the user struggled with in our prototype in hopes of improving it.
+                    </p>
+                </div>
 
-				<div className="p-10">
-					<p className="font-medium text-3xl my-10">Future Design Potential</p>
+                <div className="p-10">
+                    <p className="font-medium text-3xl my-10">Future Design Potential</p>
 
-					<p>
-						In terms of future design potential we can expand on the flow of the
-						app. We should have more ways for users to get back to specific
-						pages in our app. We can also provide more customizability in terms
-						of themes and background for the user, so they can customize the app
-						to the look they want. Some things to avoid in the future is
-						creating prototypes with deadlock states, which prevents users from
-						navigating back to any of the pages. So all pages and places our app
-						leads to should flow back to another state. Another thing about
-						designing is that you should make anything that users expect to be
-						able to click on interactable. Don’t give users text boxes or
-						buttons that don’t do anything otherwise users will think the app is
-						unresponsive, or get confused wondering why the buttons don’t work.
-						That would what we would do in the future from what we have learned
-						from designing our fitness app.
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+                    <p>
+                        In terms of future design potential we can expand on the flow of the app. We should have more ways for
+                        users to get back to specific pages in our app. We can also provide more customizability in terms of
+                        themes and background for the user, so they can customize the app to the look they want. Some things to
+                        avoid in the future is creating prototypes with deadlock states, which prevents users from navigating back
+                        to any of the pages. So all pages and places our app leads to should flow back to another state. Another
+                        thing about designing is that you should make anything that users expect to be able to click on
+                        interactable. Don’t give users text boxes or buttons that don’t do anything otherwise users will think the
+                        app is unresponsive, or get confused wondering why the buttons don’t work. That would what we would do in
+                        the future from what we have learned from designing our fitness app.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Research;
